58f463976f4ee388f9b548cfa89d96502131775f
Ganesh Ralla <ganeshrtist@gmail.com>
2026-06-19 17:33:06 +0530

    allowed hosts with no slash

--- feasthub/settings.py
+++ feasthub/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG',default=False,cast=bool)
 
 ALLOWED_HOSTS = [
-    'https://feasthub-backend.onrender.com/'
+    'https://feasthub-backend.onrender.com'
 ]
 
 
